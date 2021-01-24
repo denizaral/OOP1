0e70b3d4c0da222f24f2cece58b02dfcad334aa8
denizaral <araldeniz97@gmail.com>
2021-01-24 20:42:43 +0300

    Add project files.

---
+++ Genericsintro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genericsintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Deniz");
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+            Console.WriteLine("Deniz Aral");
+            
+        }
+    }
+}
---
+++ Genericsintro/T.cs
@@ -0,0 +1,6 @@
+﻿namespace Genericsintro
+{
+    public class T
+    {
+    }
+}
